2b951dc3ea5d92957a40f1aea076a6a5f38634b0
Trung Le <leanhtrung97@gmail.com>
2023-04-13 15:56:35 +0700

    First Init

---
+++ src/main/java/com/app/dwbros/DwBrosApplication.java
@@ -0,0 +1,13 @@
+package com.app.dwbros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwBrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwBrosApplication.class, args);
+    }
+
+}
